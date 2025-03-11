b030bdcfb862607e1718aff8d3fcb01d630e11fd
livebcdev0603 <b.ceb.0603@gmail.com>
2025-03-11 10:42:35 -0700

    updated

--- chat/views.py
+++ chat/views.py
@@ -52,34 +52,7 @@ def index(request):
         return render(request, "chat/Base.html", {'friends': friends})
 
 
-def search(request):
-    """
-    Search users page
-    :param request:
-    :return:
-    """
-    users = list(UserProfile.objects.all())
-    for user in users:
-        if user.username == request.user.username:
-            users.remove(user)
-            break
 
-    if request.method == "POST":
-        print("SEARCHING!!")
-        query = request.POST.get("search")
-        user_ls = []
-        for user in users:
-            if query in user.name or query in user.username:
-                user_ls.append(user)
-        return render(request, "chat/search.html", {'users': user_ls, })
-
-    try:
-        users = users[:10]
-    except:
-        users = users[:]
-    id = getUserId(request.user.username)
-    friends = getFriendsList(id)
-    return render(request, "chat/search.html", {'users': users, 'friends': friends})
 
 
 def addFriend(request, name):
