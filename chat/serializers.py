dfaa2571bab5eb22bc2653efc0154c12567ea043
livebcdev0603 <b.ceb.0603@gmail.com>
2025-03-17 10:01:26 +0000

    updated

--- chat/serializers.py
+++ chat/serializers.py
@@ -5,7 +5,6 @@ from .models import Messages, UserProfile
 class MessageSerializer(serializers.ModelSerializer):
 
     sender_name = serializers.SlugRelatedField(many=False, slug_field='username', queryset=UserProfile.objects.all())
-    receiver_name = serializers.SlugRelatedField(many=False, slug_field='username', queryset=UserProfile.objects.all())
 
     class Meta:
         model = Messages
